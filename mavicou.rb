85d83a4ea0727dfea89003df649514e5a6bd4a77
Thomas Deschamps <thomasdscmps@gmail.com>
2020-10-26 18:47:56 +0100

    :monkey:

--- mavicou.rb
+++ mavicou.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Toto
-  POSTMAN_KEY = 'PMAK-5e3c52895351d62230f49163-123cbbbd012251996d81dc089150e57d54'
+  POSTMAN_KEY = 'PMAK-5e3c74825351d62230f49163-123cbbbd012251996d81dc089150e57d54'
 
   def self.call
     'bmlxdWVzYW1lcmVsYXJlaW5zZXJ0aW9uCg=='
